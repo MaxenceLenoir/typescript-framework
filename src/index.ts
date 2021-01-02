66f8f6cf0b7ff5ad66e39e40ef83f7e0aacab828
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-02 10:26:06 +0100

    Framework first steps

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import {User} from './models/User';
+import { User } from './models/User';
 
-const user = new User({name: 'Max', age: 27})
-
-user.on('change', () => {
-  console.log('#1')
-});
-user.on('change', () => {
-  console.log('#2')
-});
-user.trigger('change');
+const user = new User({ name: 'NewUser', age: 35 });
